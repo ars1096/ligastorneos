aa93a75cbe073fe36554c6367f62f13ceb9751af
ars96 <arsarsars80@gmail.com>
2018-03-05 10:39:59 +0000

    app con ionic para ligas, torneos...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {MyTeamsPage} from "../pages/my-teams/my-teams";
+import {TournamentsPage} from "../pages/tournaments/tournaments";
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,7 +12,7 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = MyTeamsPage;
 
   pages: Array<{title: string, component: any}>;
 
@@ -20,10 +20,7 @@ export class MyApp {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
-    ];
+
 
   }
 
@@ -35,7 +32,13 @@ export class MyApp {
       this.splashScreen.hide();
     });
   }
+  goHome(){
+    this.nav.setRoot(MyTeamsPage);
+  }
 
+  goToTournaments(){
+    this.nav.setRoot(TournamentsPage);
+  }
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
